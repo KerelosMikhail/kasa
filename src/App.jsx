4ae7a089f44136add16ea4b71160886c0afc886f
Kerelos Mikhail <Kerelos.Adel@gmail.com>
2025-05-09 02:53:43 -0400

    Using react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { Routes, Route, Link } from "react-router-dom"; // Import routing components
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function D_Home() {
+  const [count, setCount] = useState(0);
   return (
     <>
       <div>
@@ -29,7 +29,37 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+function D_Rental_Property_Card() {
+  return <h1>D_Rental Property Card</h1>;
+}
+
+function D_404() {
+  return <h1>404 - page not found</h1>;
+}
+
+function D_About() {
+  return <h1>About Page</h1>;
+}
+
+function App() {
+  return (
+    <>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link> |{" "}
+        <Link to="/property">Rental Property</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<D_Home />} />
+        <Route path="/about" element={<D_About />} />
+        <Route path="/property" element={<D_Rental_Property_Card />} />
+        <Route path="*" element={<D_404 />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
