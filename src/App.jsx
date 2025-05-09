7c9e053d45a0e724fbca5a1ce19b15273e53cc7b
Kerelos Mikhail <Kerelos.Adel@gmail.com>
2025-05-09 03:06:41 -0400

    Adding Mobile screen

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import viteLogo from "/vite.svg";
 import { Routes, Route, Link } from "react-router-dom"; // Import routing components
 import "./App.css";
 
+function Layout({ desktop: DesktopComponent, mobile: MobileComponent }) {
+  const isMobile = window.innerWidth <= 768;
+  return isMobile ? <MobileComponent /> : <DesktopComponent />;
+}
+
 function D_Home() {
   const [count, setCount] = useState(0);
   return (
@@ -32,16 +37,32 @@ function D_Home() {
   );
 }
 
+function M_Home() {
+  return <h1>Mobile Home Layout</h1>;
+}
+
 function D_Rental_Property_Card() {
   return <h1>D_Rental Property Card</h1>;
+}
+
+function M_Rental_Property_Card() {
+  return <h1>Mobile Rental Property Card</h1>;
 }
 
 function D_404() {
   return <h1>404 - page not found</h1>;
 }
 
+function M_404() {
+  return <h1> Mobile 404 - page not found</h1>;
+}
+
 function D_About() {
   return <h1>About Page</h1>;
+}
+
+function M_About() {
+  return <h1>Mobil About Page</h1>;
 }
 
 function App() {
@@ -53,10 +74,21 @@ function App() {
       </nav>
 
       <Routes>
-        <Route path="/" element={<D_Home />} />
-        <Route path="/about" element={<D_About />} />
-        <Route path="/property" element={<D_Rental_Property_Card />} />
-        <Route path="*" element={<D_404 />} />
+        <Route path="/" element={<Layout desktop={D_Home} mobile={M_Home} />} />
+        <Route
+          path="/about"
+          element={<Layout desktop={D_About} mobile={M_About} />}
+        />
+        <Route
+          path="/property"
+          element={
+            <Layout
+              desktop={D_Rental_Property_Card}
+              mobile={M_Rental_Property_Card}
+            />
+          }
+        />
+        <Route path="*" element={<Layout desktop={D_404} mobile={M_404} />} />
       </Routes>
     </>
   );
