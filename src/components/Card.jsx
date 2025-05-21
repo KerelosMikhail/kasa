0295d904cfc6777e07bff4e320ad2f55539a9465
Kerelos Mikhail <Kerelos.Adel@gmail.com>
2025-05-21 03:21:32 -0400

    Adding gallery and card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,9 @@
 function Card() {
   return (
-    <>
-      <img src={logo} className="logo" alt="Kasa logo" />
-      <p> Title page </p>
-    </>
+    <div className="gallery-card">
+      <div className="gallery-card__gradient"></div>
+      <div className="gallery-card__text">Title of the rental property</div>
+    </div>
   );
 }
 export default Card;
---
+++ src/components/RentalPropertyCard.jsx
@@ -0,0 +1,9 @@
+function RentalPropertyCard() {
+  return (
+    <body className="RentalPropertyCard">
+      {/* <img src={logo} className="logo" alt="Kasa logo" />
+      <p> Title page </p> */}
+    </body>
+  );
+}
+export default RentalPropertyCard;
