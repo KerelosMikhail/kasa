979f31b4ba899db0f0a803b48546654ea349e5cb
Kerelos Mikhail <Kerelos.Adel@gmail.com>
2025-05-23 11:18:40 -0400

    Adding image to the card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,9 @@
+import thumbImage from "../assets/thumbImage.jpg";
+
 function Card() {
   return (
     <div className="gallery-card">
+      <img src={thumbImage} alt="PropertyImage" className="gallery-card__img" />
       <div className="gallery-card__gradient"></div>
       <div className="gallery-card__text">Title of the rental property</div>
     </div>
