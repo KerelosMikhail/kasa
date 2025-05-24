69922554acc7db05d0e0349a914046e9ecdff7ed
Kerelos Mikhail <Kerelos.Adel@gmail.com>
2025-05-23 23:59:20 -0400

    Adding images to the Carousel, next and previous arrows

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,28 +1,53 @@
 import { useState } from "react";
-import carrouselImage from "../assets/carrouselImage.webp";
 import rightArrow from "../assets/right-arrow.svg";
 import leftArrow from "../assets/left-arrow.svg";
 
-function Carousel() {
-  // For demonstration, assume 4 images and current index is 1
-  const [current, setCurrent] = useState(1);
-  const total = 4;
+function Carousel({ images = [] }) {
+  const total = images.length;
+  const [current, setCurrent] = useState(0);
+
+  if (total === 0) return null;
+
+  const goPrev = () =>
+    setCurrent((prev) => (prev === 0 ? total - 1 : prev - 1));
+  const goNext = () =>
+    setCurrent((prev) => (prev === total - 1 ? 0 : prev + 1));
 
   return (
     <div className="carousel">
-      <img src={carrouselImage} alt="Property" className="carousel__image" />
-      {/* Left Arrow */}
-      <div className="carousel__arrow-container carousel__arrow-container--left">
-        <img src={leftArrow} alt="Previous" className="carousel__arrow" />
-      </div>
-      {/* Right Arrow */}
-      <div className="carousel__arrow-container carousel__arrow-container--right">
-        <img src={rightArrow} alt="Next" className="carousel__arrow" />
-      </div>
-      {/* Image Counter */}
-      <div className="carousel__counter">
-        {current}/{total}
-      </div>
+      <img
+        src={images[current]}
+        alt={`Property ${current + 1}`}
+        className="carousel__image"
+      />
+      {total > 1 && (
+        <>
+          {/* Left Arrow */}
+          <div
+            className="carousel__arrow-container carousel__arrow-container--left"
+            onClick={goPrev}
+            role="button"
+            tabIndex={0}
+            aria-label="Previous image"
+          >
+            <img src={leftArrow} alt="Previous" className="carousel__arrow" />
+          </div>
+          {/* Right Arrow */}
+          <div
+            className="carousel__arrow-container carousel__arrow-container--right"
+            onClick={goNext}
+            role="button"
+            tabIndex={0}
+            aria-label="Next image"
+          >
+            <img src={rightArrow} alt="Next" className="carousel__arrow" />
+          </div>
+          {/* Image Counter */}
+          <div className="carousel__counter">
+            {current + 1}/{total}
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- src/pages/RentalPropertyPage.jsx
+++ src/pages/RentalPropertyPage.jsx
@@ -1,9 +1,17 @@
 import Carousel from "../components/Carousel";
 
 function RentalPropertyPage() {
+  const images = [
+    "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-16-1.jpg",
+    "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-16-2.jpg",
+    "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-16-3.jpg",
+    "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-16-4.jpg",
+    "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-16-5.jpg",
+  ];
+
   return (
     <div className="rental-property-page">
-      <Carousel />
+      <Carousel images={images} />
     </div>
   );
 }
